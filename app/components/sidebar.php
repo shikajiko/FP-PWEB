bae9d5a2cda5c332f740ab3cc93edf66472269f0
shikajiko <riyanfadli6675@gmail.com>
2025-11-30 14:20:19 +0700

    project structure

---
+++ app/components/sidebar.php
@@ -0,0 +1,11 @@
+<div class="container d-flex flex-column">
+    <div>
+        item 1
+    </div>
+    <div>
+        item 2
+    </div>
+    <div>
+        item 3
+    </div>
+</div>
---
+++ app/src/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../styles/globals.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-auto">
+                <?php include "components/sidebar.php"; ?>
+            </div>
+            <div class="col d-flex flex-column">
+                <div>
+                    insert header here
+                </div>
+                <div>
+                    main content here
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
