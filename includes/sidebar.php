d70917def14842ef28a0f63e683c9d658087fc52
shikajiko <riyanfadli675@gmail.com>
2025-11-30 22:20:47 +0700

    slicing sidebar

---
+++ includes/sidebar.php
@@ -0,0 +1,26 @@
+<div class="sidebar pt-3">
+    <div class="d-flex flex-column justify-content-between h-100">
+        <div class="mt-4">
+            <div class="sidebar-logo mb-5 d-flex flex-row justify-content-center align-items-center">
+                <img src="img/logo.png" alt="">
+            </div>
+            <div class="ps-5 sidebar-item gap-3 py-3 d-flex flex-row justify-content-start align-items-center">
+                <img src="img/dashboard-icon.svg" alt="icon">
+                <a href=""> Dashboard</a>
+            </div>
+            <div class="ps-5 sidebar-item gap-3 py-3 d-flex flex-row justify-content-start align-items-center">
+                <img src="img/courses-icon.svg" alt="icon">
+                <a href=""> Courses</a>
+            </div>
+            <div class="ps-5 sidebar-item gap-3 py-3 d-flex flex-row justify-content-start align-items-center">
+                <img src="img/calendar-icon.svg" alt="icon">
+                <a href=""> Calendar</a>
+            </div>
+        </div>
+        
+        <div class="ps-5 d-flex flex-row justify-content-start align-items-center">
+                <img src="" class="rounded" alt="profile-picture">
+                <p>Naufal</p>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../styles/globals.css">
+    <link rel="stylesheet" href="styles/globals.css">
+        <link rel="stylesheet" href="styles/sidebar.css">
     <title>Dashboard</title>
 </head>
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-auto">
-                <?php include "components/sidebar.php"; ?>
+    <div class="container-fluid p-0">
+        <div class="row p-0">
+            <div class="col-auto p-0 overflow-hidden">
+                <?php include "includes/sidebar.php"; ?>
             </div>
             <div class="col d-flex flex-column">
                 <div>
