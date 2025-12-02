a960a66dd6ffb484b74b3b74d945d31b007012a2
shikajiko <riyanfadli675@gmail.com>
2025-12-02 21:29:54 +0700

    slicing dashboard

---
+++ includes/course-card.php
@@ -0,0 +1,9 @@
+<div class="course-card d-flex flex-column">
+    <div class="py-0 my-0">
+        <img src="img/course-bg-1.jpg" class="py-0 my-0" alt="">
+    </div>
+    <div class="course-title px-3 py-3 gap-0 d-flex flex-column">
+        <p class="py-0 my-0">Semester Gasal 2025/2026</p>
+        <h5 class="py-0 my-0">Pemrograman Web</h5>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -5,25 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/globals.css">
-    <link rel="stylesheet" href="styles/sidebar.css">
-    <link rel="stylesheet" href="styles/header.css">
     <title>Dashboard</title>
 </head>
 <body>
-    <div class="container-fluid p-0">
-        <div class="row p-0">
-            <div class="col-auto p-0 overflow-hidden">
-                <?php include "includes/sidebar.php"; ?>
-            </div>
-            <div class="col d-flex flex-column ps-3 pe-5">
-                <div>
-                    <?php include "includes/header.php"; ?>
-                </div>
-                <div>
-                    main content here
-                </div>
-            </div>
-        </div>
+    <div class="container-fluid p-0 main-container">
+  <div class="row p-0">
+    <div class="col-auto p-0">
+      <?php include "includes/sidebar.php"; ?>
     </div>
+    <div class="col d-flex flex-column px-5">    
+      <?php include "includes/header.php"; ?>
+      <div class="d-flex flex-row gap-5 w-100"> 
+        <div class="d-flex flex-column" style="flex: 2 1 0;">
+          <div class="mt-5">
+            <?php include "includes/carousel.php"; ?>
+          </div>
+          <div class="courses-container d-flex flex-column mt-5 px-4 pt-3 pb-4">
+            <h2>My Courses</h2>
+            <div class="d-flex flex-row justify-content-between gap-3">
+              <?php include "includes/course-card.php"; ?>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex flex-column home-right-panel py-3 mt-4" style="flex: 1 1 0;">
+          <div></div>
+          <div class="task-container d-flex flex-column"></div>
+        </div>
+      </div>                 
+    </div>
+  </div>
+</div>
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </html>
